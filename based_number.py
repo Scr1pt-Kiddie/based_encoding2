43b2d6ce95dab0d0b11f0a90c30bdd1278a0768d
Scr1ptKiddie <37203375+Scr1pt-Kiddie@users.noreply.github.com>
2024-09-09 00:19:25 +0500

    Update based_number.py
    explain, abd some other stuff

--- based_number.py
+++ based_number.py
@@ -1,5 +1,4 @@
-from dataclasses import dataclass
-
+# Простые исключения для ошибок
 class BaseTooSmall(Exception):
     pass
 
@@ -9,33 +8,36 @@ class ValueBiggerThanBase(Exception):
 class IntegerIsSigned(Exception):
     pass
 
-class UnsignedInt(Exception):
+# Класс для положительных целых чисел
+class UnsignedInt:
     def __init__(self, value: int) -> None:
         if value < 0:
-            raise IntegerIsSigned()
-        self._value = value
+            raise IntegerIsSigned()  # Ошибка, если число отрицательное
+        self._value = value  # Сохраняем число
 
     @property
     def value(self) -> int:
-        return self._value
+        return self._value  # Возвращаем значение
 
+# Класс для баз (основ) систем счисления
 class ValidBase:
     def __init__(self, value: UnsignedInt) -> None:
         if value.value < 1:
-            raise BaseTooSmall()
-        self._value = value
+            raise BaseTooSmall()  # Ошибка, если база меньше 1
+        self._value = value  # Сохраняем базу
 
     @property
     def value(self) -> UnsignedInt:
-        return self._value
+        return self._value  # Возвращаем базу
 
     def increase(self, n: UnsignedInt):
-        self._value._value += n.value
+        self._value._value += n.value  # Увеличиваем значение базы
 
+# Класс для цифр с учетом их базы
 class BasedDigit:
     def __init__(self, base: ValidBase, value: UnsignedInt) -> None:
         if base.value.value <= value.value:
-            raise ValueBiggerThanBase()
+            raise ValueBiggerThanBase()  # Ошибка, если цифра больше базы
         self._base = base
         self._value = value
 
@@ -48,47 +50,49 @@ class BasedDigit:
         return self._value
 
     def increase(self, n: UnsignedInt):
-        self._value._value += n.value
-        self._base._value._value += n.value
+        self._value._value += n.value  # Увеличиваем цифру
+        self._base._value._value += n.value  # Увеличиваем базу
 
-@dataclass
+# Класс для работы с числами
 class BasedNumber:
     def __init__(self) -> None:
-        self._digits: list[UnsignedInt] = []
-        self._base: ValidBase = ValidBase(UnsignedInt(1))
+        self._digits = []  # Список для цифр
+        self._base = ValidBase(UnsignedInt(1))  # Начальная база
 
     def digits(self):
-        return iter(self._digits)
+        return iter(self._digits)  # Возвращаем список цифр
 
     @property
     def base(self):
-        return self._base
+        return self._base  # Возвращаем текущую базу
 
     def convert(self, new_base: ValidBase):
         if new_base == self.base:
-            return
+            return  # Ничего не делаем, если база не меняется
         self_as_number = 0
 
+        # Преобразуем число в десятичную форму
         for index, digit in enumerate(self._digits[::-1]):
             self_as_number += self._base.value.value ** index * digit.value
 
+        # Преобразуем число в новую базу
         reversed_digits = []
         while self_as_number:
             reversed_digits.append(UnsignedInt(self_as_number % new_base.value.value))
             self_as_number //= new_base.value.value
 
-        self._base = new_base
-        self._digits = reversed_digits[::-1]
+        self._base = new_base  # Меняем базу
+        self._digits = reversed_digits[::-1]  # Сохраняем цифры
 
     def write(self, based_digit: BasedDigit):
         if len(self._digits) == 0 and based_digit.value.value == 0:
-            return
-        self.convert(based_digit.base)
-        self._digits.append(based_digit.value)
+            return  # Не записываем нули, если число пустое
+        self.convert(based_digit.base)  # Преобразуем в базу цифры
+        self._digits.append(based_digit.value)  # Добавляем цифру
 
     def read_digit(self, base: ValidBase) -> UnsignedInt:
-        self.convert(base)
+        self.convert(base)  # Преобразуем в нужную базу
         try:
-            return self._digits.pop()
+            return self._digits.pop()  # Возвращаем последнюю цифру
         except IndexError:
-            return UnsignedInt(0)
+            return UnsignedInt(0)  # Если цифр нет, возвращаем 0
